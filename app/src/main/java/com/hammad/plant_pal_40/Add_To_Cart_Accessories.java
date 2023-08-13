235af7beaa8477ea940005a5a816a6c618066388
HP <khan.hamm23kk@gmail.com>
2023-08-13 16:50:27 +0500

    updated

---
+++ app/src/main/java/com/hammad/plant_pal_40/Add_To_Cart_Accessories.java
@@ -0,0 +1,14 @@
+package com.hammad.plant_pal_40;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_To_Cart_Accessories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_to_cart_accessories);
+    }
+}
